5edc6094509bb3f9c57513f9a6d414f4678a2b0b
jabedhasan21 <jabedhasan21@gmail.com>
2015-01-13 01:55:00 +0600

    update address response

--- app/controllers/AddressController.php
+++ app/controllers/AddressController.php
@@ -6,28 +6,36 @@ class AddressController extends \BaseController {
 	public function index(){
 
 		$response = new stdClass();
-		$countryArray = array();
+		$divisionArray = array();
 
 		$country = Country::find(1);
-		$divisions = $country->divisions->toArray();
+		$divisions = $country->divisions;
 
 		foreach($divisions as $division  ){
+			
+			$divisionObj = Division::find($division->id);
+			$districts = $divisionObj->districts;
 
-			$tempDistrict = array();
-
-			$divisionObj = Division::find($division['id']);
-			$districts = $divisionObj->districtss->toArray();
-
+			$tempDivision = array();
 			foreach($districts as $district  ){
-
-				$districtObj = District::find($district['id']);
-				$upzillas = $districtObj->upzillas->toArray();
-
-				$tempDistrict = array($district,'upozillas'=>$upzillas );
+				$districtObj = District::find($district->id);
+				$upzillas = $districtObj->upzillas;
+				
+				$temp = new stdClass;
+				$temp->id = $district->id;
+				$temp->name = $district->name;
+				$temp->upzillas = $upzillas;
+				array_push($tempDivision, $temp);
 			}
-			array_push($countryArray, array($division,'districts'=>$tempDistrict));
+			$tempC = new stdClass;
+			$tempC->id = $division->id;
+			$tempC->name = $division->name;
+			$tempC->districts = $tempDivision;
+			array_push($divisionArray, $tempC);
 		}
-		$response->all_address = $countryArray;
+		$response->id = $country->id;
+		$response->name = $country->name;
+		$response->divisions = $divisionArray;
 		return json_encode($response);
 	}
 
